02a27567366c2ebda58eab2a89989bc8ecba8937
Javier Maravilla <javier.maravilla@outlook.com>
2021-10-12 15:55:53 -0400

    Second commit

---
+++ flatiron-bakes/src/App.js
@@ -0,0 +1,14 @@
+import CakeCard from "./CakeCard";
+
+function App() {
+  return (
+    <>
+    <h1>Bakery Name</h1>
+    <CakeCard flavor={"Carrot and Walnut Cake"} price={20}/>
+    <CakeCard flavor={"Chocolate"} price={25}/>
+    <CakeCard flavor={"Vanilla"} price={28}/>
+    </>
+  );
+}
+
+export default App;
